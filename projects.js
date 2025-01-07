6bc65a01982dc16aafbf51d87445340e0fb43dd6
Israel Trejo <isrtrejo07@gmail.com>
2025-01-07 15:56:36 -0600

    Added a JavaScript file, deleted contact button

---
+++ projects.js
@@ -0,0 +1,40 @@
+const project = [
+    {
+        thumbnail: "Assets/real-project-4-amazon.png",
+        name: "Amazon Project",
+        skills: [
+            "./Skills/JS-Skill.jpg",
+            "./Skills/html-skill.jpg",
+            "./Skills/CSS-skill.jpg"
+        ],
+        link: "https://github.com/Israeli21/amazon-project-main"
+    },
+    {
+        thumbnail: "Assets/portfolio-website.png",
+        name: "Portfolio Website",
+        skills: [
+            "./Skills/JS-Skill.jpg",
+            "./Skills/html-skill.jpg",
+            "./Skills/CSS-skill.jpg"
+        ],
+        link: "https://github.com/Israeli21/html-css-js-portfolio-tutorial"
+    },
+    {
+        thumbnail: "Assets/real-project-1.png",
+        name: "Youtube Homepage",
+        skills: [
+            "./Skills/JS-Skill.jpg",
+            "./Skills/html-skill.jpg",
+            "./Skills/CSS-skill.jpg"
+        ],
+        link: "https://github.com/Israeli21/youtube-project"
+    },
+    {
+        thumbnail: "Assets/real-project-3.png",
+        name: "Snake Game",
+        skills: [
+            "./Skills/cpp-skill.jpg",
+        ],
+        link: "https://github.com"
+    }
+];
