435f40db488b08ed055ac3e9669ede15f448c5b6
Israel Trejo <isrtrejo07@gmail.com>
2025-01-11 18:47:01 -0600

    Added scroll feature to project containers

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
+const container = document.querySelector('.projects-scroll-container');
+const projectContainerWidth = container.querySelector('.project-container').offsetWidth;
+
+buttonRight.onclick = function () {
+  container.scrollLeft += projectContainerWidth + 16; // Adjust for the gap if any
+};
+
+buttonLeft.onclick = function () {
+  container.scrollLeft -= projectContainerWidth + 16; // Adjust for the gap if any
+};
